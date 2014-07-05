cdd204161c6c06cea7da13da27e4dd03221d7a78
arashgit <arash_7mh@yahoo.com>
2014-07-05 14:32:24 +1000

    Create index.php

---
+++ image-content/index.php
@@ -0,0 +1,13 @@
+<?php
+// do direct access allowed
+header('HTTP/1.0 404 not found');  
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+</head>
+<body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+</body>
+</html>
