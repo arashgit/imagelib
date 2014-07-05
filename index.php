9a0aec26f7afbdbf6000e784415d78cc1b14568c
arashgit <arash_7mh@yahoo.com>
2014-07-05 14:16:48 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// index.php
+// do not run this file
+// run test.php instead
+header('HTTP/1.0 404 not found');  
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+</head>
+<body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+</body>
+</html>
