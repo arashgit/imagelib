d2dbae7b2e8564c44221fcc92f10a3d3c956aba5
arashgit <arash_7mh@yahoo.com>
2014-07-05 14:29:54 +1000

    Create custom2.php

---
+++ image-usereffects/custom2.php
@@ -0,0 +1,38 @@
+<?php
+// image-usereffects/custom2.php
+
+if ( ! defined('IMAGEAPI_ROOT'))
+	exit('No direct script access allowed');
+
+class CustomEffect2
+{
+	// user custom effect
+	// effect:
+	// 		effect1
+	// prameters
+	// 		(none)
+	public static function effect1($source_image,$parameters)
+	{
+		if(IMAGEAPI_ENVIRONMENT=='development')
+			echo 'CustomEffect2::effect1 is called but not implemented yet.<br>';
+		else
+			throw new Exception("not implemented", 1);
+		
+		return $source_image;
+	}
+
+	// user custom effect
+	// effect:
+	// 		effect2
+	// prameters
+	// 		(none)
+	public static function effect2($source_image,$parameters)
+	{
+		if(IMAGEAPI_ENVIRONMENT=='development')
+			echo 'CustomEffect2::effect2 is called but not implemented yet.<br>';
+		else
+			throw new Exception("not implemented", 1);
+
+		return $source_image;
+	}
+}
